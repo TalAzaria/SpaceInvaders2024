5e68059d6c2518cdea07cf5f0f038fe9af50c875
tal122 <talazaria2@gmail.com>
2024-12-05 12:10:11 +0200

    Work

--- Assets/Scripts/EnemiesControl.cs
+++ Assets/Scripts/EnemiesControl.cs
@@ -1,45 +1,70 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.EventSystems.EventTrigger;
 
 public class EnemiesControl: MonoBehaviour
 {
     [SerializeField] private List<Enemy> enemiesPrefabs = new();
-    [SerializeField] private int rows, columns;
+    [SerializeField] private int rows, columns, numberOfMoves;
     private Enemy[,] enemies;
     [SerializeField] private float speed = 1;
-    [SerializeField] private float moveDistance = 0.2f;
+    [SerializeField] private Vector2 moveDistance = new Vector2(0.4f, 0.8f);
     [SerializeField] private Transform topLeftSpawnPoint, bottomRightSpawnPoint;
 
     private void Awake()
     {
         enemies = new Enemy[rows, columns];
+        OnScreenResolutionChanged(new Vector2(Screen.width, Screen.height));
+    }
+
+    private void Start()
+    {
+        ScreenManager.Instance.AddScreenBoundedObject(topLeftSpawnPoint);
+        ScreenManager.Instance.AddScreenBoundedObject(bottomRightSpawnPoint);
+        ScreenManager.Instance.OnScreenResolutionChanged += OnScreenResolutionChanged;
+    }
+
+    private void OnScreenResolutionChanged(Vector2 newResolution)
+    {
+
     }
 
     public IEnumerator MoveEnemies()
     {
-        int numberOfMoves = (int)((ScreenManager.Instance.ScreenWorldBounds.size.x - 
-            Mathf.Abs(bottomRightSpawnPoint.position.x + topLeftSpawnPoint.position.x - 
-            2 * (ScreenManager.Instance.ScreenWorldBounds.center.x - ScreenManager.Instance.ScreenWorldBounds.extents.x))) / moveDistance);
-        Debug.Log(numberOfMoves);
-        yield return MoveHorizontaly(Vector2.right, numberOfMoves);
-        yield return MoveHorizontaly(Vector2.down, 1);
-        yield return MoveHorizontaly(Vector2.left, numberOfMoves);
+
+        while (true)
+        {
+            yield return MoveHorizontaly(Vector2.right * moveDistance.x, numberOfMoves);
+            yield return MoveHorizontaly(Vector2.down * moveDistance.y, 1);
+            yield return MoveHorizontaly(Vector2.left * moveDistance.x, numberOfMoves);
+            yield return MoveHorizontaly(Vector2.down * moveDistance.y, 1);
+        }
     }
 
     private IEnumerator MoveHorizontaly(Vector2 moveVector, int moveAmount)
     {
-        for (int k = 0; k < moveAmount; k++)
+        for (int k = 0; k < numberOfMoves; k++)
         {
             for (int i = 0; i < rows; i++)
             {
                 for (int j = 0; j < columns; j++)
                 {
-                    enemies[rows - i - 1, j].gameObject.transform.position += (Vector3)moveVector * moveDistance;
+                    enemies[rows - i - 1, j].gameObject.transform.position += (Vector3)moveVector;
                     yield return new WaitForSeconds(1 / speed);
                 }
             }
         }
+
+        numberOfMoves = (int)((ScreenManager.Instance.ScreenWorldBounds.size.x -
+            Mathf.Abs(bottomRightSpawnPoint.position.x + topLeftSpawnPoint.position.x -
+            2 * (ScreenManager.Instance.ScreenWorldBounds.center.x - ScreenManager.Instance.ScreenWorldBounds.extents.x))) / moveDistance.x);
+    }
+
+    private void UpdateNumberOfHorizontalMove()
+    {
+
     }
 
     public void SpawnEnemies()
